a4cc9e59f5c9da1ed9ac3770614b283bf7eae437
Kavya Sampath Kumar <ksampathkumar@zuora.com>
2023-03-14 15:30:52 +0530

    post,put,delete mapping added

--- src/main/java/learn/rest/springbootrestapi/controller/StudentController.java
+++ src/main/java/learn/rest/springbootrestapi/controller/StudentController.java
@@ -1,9 +1,15 @@
 package learn.rest.springbootrestapi.controller;
 
 import learn.rest.springbootrestapi.bean.Student;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -23,8 +29,8 @@ public class StudentController {
         List<Student> students = new ArrayList<>();
         students.add(new Student(1, "Premalatha", "Chandran"));
         students.add(new Student(2, "John", "Doe"));
-        students.add(new Student(3, "Rajesh", "Dayalan"));
-        students.add(new Student(4, "Sathish", "Dayalan"));
+        students.add(new Student(3, "Jack", "Rose"));
+        students.add(new Student(4, "Sathish", "Chaterjee"));
         return students;
     }
 
@@ -43,5 +49,32 @@ public class StudentController {
     @GetMapping("students/query")
     public Student studentRequesVariable(@RequestParam int id, @RequestParam String firstName, @RequestParam String lastName) {
         return new Student(id, firstName, lastName);
+    }
+
+    // SprintBoot REST API that handles HTTP POST Request - creating new resource
+    // Post Mapping and RequestBody
+    @PostMapping("students/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent(@RequestBody Student student) {
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
+    }
+
+    //Spring boot REST API that hansdles HTTP PUT request - updating existing resource
+    @PutMapping("students/{id}/update")
+    public Student updateStudent(@RequestBody Student student, @PathVariable("id") int studentId) {
+        System.out.println(student.getId());
+        System.out.println(student.getId());
+        System.out.println(student.getId());
+        return student;
+    }
+
+    //Springboot REST API that handles HTTP Delete request
+    @DeleteMapping("students/{id}/delete")
+    public String deleteStudent(@PathVariable("id") int studentId) {
+        System.out.println(studentId);
+        return "Student deleted successfully";
     }
 }
